e2fcdf58c8d2934a0c3054cf69a70e9bc1e58b7d
Daniyal1229 <syeddaniyalbokhari72@gmail.com>
2023-05-05 20:21:34 +0530

    Employee Record Management System using php

---
+++ erms/admin/allemployees.php
@@ -0,0 +1,173 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+//error_reporting(0);
+if (strlen($_SESSION['aid']==0)) {
+  header('location:logout.php');
+  } else{
+
+
+if(isset($_GET['delid']))
+  {
+    $eid=$_GET['delid'];
+    $query=mysqli_query($con,"delete employeedetail,empexpireince,empeducation from employeedetail
+left join empexpireince on empexpireince.EmpID=employeedetail.ID
+left join empeducation on empeducation.EmpID=employeedetail.ID
+where employeedetail.ID='$eid'");
+    echo "<script>alert('Record Deleted successfully');</script>";
+    echo "<script>window.location.href='allemployees.php'</script>";
+  }
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Employees Details</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+  <?php include_once('includes/sidebar.php')?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content"style="background-color:black;color:white">
+
+        <!-- Topbar -->
+         <?php include_once('includes/header.php')?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-lg-800">Employees Details</h1>
+
+<p style="font-size:16px; color:red" align="center"> <?php if($msg){
+    echo $msg;
+  }  ?> </p>
+
+ <div class="table-responsive">
+<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
+<tr>
+  <th>S no.</th>
+  <th>Emp Code</th>
+  <th>Emp First Name</th>
+  <th>Emp Last Name</th>
+  <th>Emp Email</th>
+  <th>Emp Contact no</th>
+  <th>Emp Joining Date</th>
+  <th>Action</th>
+  
+</tr>
+
+<?php
+$ret=mysqli_query($con,"select * from employeedetail");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+
+<tr>
+  <td><?php echo $cnt;?></td>
+  <td><?php  echo $row['EmpCode'];?></td>
+   <td><?php echo $row['EmpFname'];?></td>
+    <td><?php echo $row['EmpLName'];?></td>
+  <td><?php echo $row['EmpEmail'];?></td>
+  <td><?php echo $row['EmpContactNo'];?></td>
+  <td><?php echo $row['EmpJoingdate'];?></td>
+  <td><a href="editempprofile.php?editid=<?php echo $row['ID'];?>"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="50" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg></a> 
+    <a href="editempexp.php?editid=<?php echo $row['ID'];?>"style="color:green"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="50" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+  <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+  <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+</svg></a>  
+    <a href="allemployees.php?delid=<?php echo $row['ID'];?>" onclick="return confirm('Do you really want to delete');" style="color:red"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="50" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+  <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+</svg></a>
+  </td>
+</tr>
+
+<?php 
+$cnt=$cnt+1;
+}?>
+
+</table>
+
+</div>
+
+
+
+
+
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+   <?php include_once('includes/footer.php');?>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../js/sb-admin-2.min.js"></script>
+  
+  <!-- Page level plugins -->
+  <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="../js/demo/datatables-demo.js"></script>
+</body>
+
+</html>
+<?php }  ?>
---
+++ erms/admin/includes/footer.php
@@ -0,0 +1,7 @@
+   <footer class="sticky-footer bg-black" style="background-color:black">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy;  EMS Project 2023 By Daniyal Syed</span>
+          </div>
+        </div>
+      </footer>
